d79794947bb69a1b6a59372bd43bad61f485c62c
thanghq12 <thanghq12@fpt.edu.vn>
2024-01-16 17:12:35 +0700

    update

--- bai3/labkt.php
+++ bai3/labkt.php
@@ -13,3 +13,51 @@
 /// tạo phương thức tính tổng lương = luongCB *soGioDay
 /// //tạo phương thức hiển thị thông tin giảng viên hiển thị ra các thông tin
 //// hoten,diachi,tuoi,email,sdt,tổng lương
+class ConNguoi {
+    public $hoTen;
+    public $diaChi;
+    public $sdt;
+    public $namSinh;
+    public $email;
+    public function __construct($hoTen,$diaChi,$sdt,$namSinh,$email)
+    {
+        $this->hoTen = $hoTen;
+        $this->diaChi = $diaChi;
+        $this->sdt = $sdt;
+        $this->namSinh = $namSinh;
+        $this->email = $email;
+    }
+    public  function tinhTuoi() {
+        return date('Y') - $this->namSinh;
+    }
+    public function hienThiThongTin() {
+        echo $this->hoTen."-".$this->diaChi."-".$this->tinhTuoi();
+    }
+
+}
+
+class HocSinh extends ConNguoi {
+    public $diemToan;
+    public $diemLy;
+    public $diemHoa;
+    public function __construct($hoTen,$diaChi,$sdt,$namSinh,$email,$diemToan,$diemLy,$diemHoa)
+    {
+        parent::__construct($hoTen,$diaChi,$sdt,$namSinh,$email);
+        $this->diemToan = $diemToan;
+        $this->diemLy = $diemLy;
+        $this->diemHoa = $diemHoa;
+    }
+    public function tinhDiemTB() {
+        return ($this->diemToan + $this->diemLy + $this->diemHoa) /3;
+    }
+    public function hienThongTinSv() {
+        $this->hienThiThongTin();
+       echo "điểm TB". $this->tinhDiemTB();
+    }
+}
+$hs1 = new  HocSinh("a","tbc","abc@gmail.com",2000,"djdjd",7,8,9);
+$hs1->hienThongTinSv();
+//assGD1 :
+//thực hiện chuyển từ mô hình MVC không có class sang mô hình MVC có class
+// và áp dụng được tính chất kế thừa trong mô hình
+// thực hiện đc full chức năng thêm sửa,xóa ,danh sách
